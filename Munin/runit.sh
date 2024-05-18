c80d0ce137d77b580df1b3213d07f96c92a1e675
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2024-05-18 11:21:07 +0200

    initial commit

---
+++ Munin/runit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run -d --name=MuninServer --rm -p 192.168.0.201:8888:8888  \
+    -e ALLOWED_HOSTS="0/0" \
+    -e HOSTNAME="salaserver" \
+    -e TZ="Europe/Berlin" \
+    -v /srv/data/docker_data/Munin/munin-server/www:/var/cache/munin/www:rw \
+    -v /srv/data/docker_data/Munin/munin-server/rrd:/var/lib/munin:rw \
+    -v /srv/data/docker/Munin/conf/munin-conf.d:/etc/munin/munin-conf.d:rw \
+    -v /:/rootfs:ro \
+    -v /sys:/sys:ro munin
