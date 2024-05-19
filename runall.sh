3af741d48dec237584d7ca699cabe2c9e43d144b
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2024-05-19 12:09:51 +0200

    runall script

--- runall.sh
+++ runall.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
-sleep 60
+local_ip="10.192.168.201"
+dest_ip="10.14.0.5"
+public_server="salauser@linuxadmin.sk"
+
+#params: source_port, dest_port
+function do_port_forward {
+  local port
+  port=`ss -nltp | grep "$local_ip:$1" | cut -d ":" -f 2 | cut -d " " -f 1`
+  #do port_forward only when there is still not active port_forward for source port
+  if [[ "$port" != "$1" ]]; then
+    logger "RUNALL: Port forward for $1 port"
+    ssh -N -R $dest_ip:$2:$local_ip:$1 $public_server &
+  fi
+}
+
+##sleep delay only when running after boot
+logger "RUNALL: script started at `date`"
+if [ "$(id -u)" -eq "0" ]; then
+  sleep 60
+  logger "RUNALL: sleep delay finished at `date`" 
+fi
+
+#start of containers
 cd /srv/data/docker
 Munin/runit.sh
 
 ##port fowards
-sleep 5
+logger "RUNALL: port forward at `date`" 
+sleep 1
 #Munin
-ssh -N -R 10.14.0.5:8888:10.192.168.201:8888 salauser@linuxadmin.sk &
+do_port_forward "8888" "8888"
+
+logger "RUNALL: finished at `date`" 
