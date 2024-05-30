a8722d6ab5d4638ab16a1b4953e6290afd017a01
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2024-05-30 18:09:31 +0200

    Nextcloud

--- Nextcloud/runit.sh
+++ Nextcloud/runit.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 cd /srv/data/docker/Nextcloud
+#- doplnit vsetky volumes
+#https://github.com/nextcloud/all-in-one#how-to-change-the-default-location-of-nextclouds-datadir
+
+#local     nextcloud_aio_apache
+#local     nextcloud_aio_database
+#local     nextcloud_aio_database_dump
+#local     nextcloud_aio_mastercontainer
+#local     nextcloud_aio_nextcloud
+#local     nextcloud_aio_nextcloud_datadir
+#local     nextcloud_aio_redis
+
+#docker create volume 
+#3ae9c236f2805863491f2c03d50b70408bbb9b67eb1bb739
+
+docker volume create --driver local \
+    --opt device=/srv/data/docker_data/Nextcloud/database \
+    --opt o=bind \
+    --opt type=none \
+    nextcloud_aio_database
+
+docker volume create --driver local \
+    --opt device=/srv/data/docker_data/Nextcloud/database_dump \
+    --opt o=bind \
+    --opt type=none \
+    nextcloud_aio_database_dump
+
+docker volume create --driver local \
+    --opt device=/srv/data/docker_data/Nextcloud/nextcloud \
+    --opt o=bind \
+    --opt type=none \
+    nextcloud_aio_nextcloud
+
 docker compose -f docker-compose.yaml up -d $@
