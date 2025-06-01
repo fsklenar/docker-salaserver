75fe762cb66343d8a19fc3c1a7a24e16f0402b73
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2025-06-01 15:36:01 +0200

    docker

---
+++ Immich/run-sync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 1200
+/home/feri/docker/Immich/runit.sh 202[4-9]*
--- Immich/runit.sh
+++ Immich/runit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -it -v /mnt/backup/NAS1/Foto:/import:ro -e \
+docker run -i -v /mnt/backup/NAS1/Foto:/import:ro -e \
  IMMICH_INSTANCE_URL=http://192.168.0.202:2283/api \
  -e IMMICH_API_KEY=$IMMICH_API_KEY \
- ghcr.io/immich-app/immich-cli:latest upload -a -c 4 --dry-run "$1"
+ ghcr.io/immich-app/immich-cli:latest upload -a -c 4 "$1"
